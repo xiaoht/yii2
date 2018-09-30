f7a122de9916ff21cfe365dfcf893fad3c5973e9
DESKTOP-EB9MSBQ\Admin <xht18721626070@gmail.com>
2018-09-30 16:09:24 +0800

    模板层引入

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,13 +12,26 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'statics/css/site.css',
-        'statics/css/font-awesome-4.4.0/css/font-awesome.min.css',
+        'statics/css/bootstrap.css',
+        'statics/scripts/ionicons/css/ionicons.min.css',
+        'statics/scripts/toast/jquery.toast.min.css',
+        'statics/scripts/owlcarousel/dist/assets/owl.carousel.min.css',
+        'statics/scripts/owlcarousel/dist/assets/owl.theme.default.min.css',
+        'statics/scripts/magnific-popup/dist/magnific-popup.css',
+        'statics/scripts/sweetalert/dist/sweetalert.css',
+        'statics/css/style.css',
+        'statics/css/skins/all.css',
     ];
     public $js = [
-    ];
-    public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'statics/js/jquery.min.js',
+        'statics/js/jquery.migrate.js',
+        'statics/scripts/bootstrap/bootstrap.min.js',
+        'statics/scripts/jquery-number/jquery.number.min.js',
+        'statics/scripts/owlcarousel/dist/owl.carousel.min.js',
+        'statics/scripts/magnific-popup/dist/jquery.magnific-popup.min.js',
+        'statics/scripts/easescroll/jquery.easeScroll.js',
+        'statics/scripts/sweetalert/dist/sweetalert.min.js',
+        'statics/scripts/toast/jquery.toast.min.js',
+        'statics/js/e-magz.js',
     ];
 }
